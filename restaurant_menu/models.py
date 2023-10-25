1f777284a921852df43ba512a97e73c7309d4505
macbookpro <muhlis.asri@gmail.com>
2023-10-25 13:03:52 +0800

    initial commit

---
+++ restaurant_menu/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main Dishes"),
+    ("desserts", "Desserts")
+)
+
+STATUS = (
+    (0, "Unavailable"),
+    (1, "Available")
+)
+
+
+class Item(models.Model):
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=10000)
+    price = models.IntegerField()
+    meal_type = models.CharField(max_length=100, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__ (self):
+        return self.meal
+
+
+
